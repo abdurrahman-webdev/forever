4f1d5d7376eb416255247967ace702e4a897c7f3
abdurrahman482937 <abdurrahmanass2024@gmail.com>
2026-01-02 23:29:20 +0600

    refactor: update token retrieval in adminAuth and auth middleware

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -1,24 +1,30 @@
 import jwt from "jsonwebtoken";
 
 const adminAuth = async (req, res, next) => {
-    try {
-        const token = req.headers.authorization?.split(" ")[1];
+  try {
+    const token = req.headers.token || req.headers.authorization?.split(" ")[1];
 
-        if (!token) {
-            return res.json({ success: false, message: "Not Authorized, Login Again" });
-        }
-
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        if (decoded.email !== process.env.ADMIN_EMAIL) {
-            return res.json({ success: false, message: "Not Authorized, Login Again" });
-        }
-
-        next();
-    } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: error.message });
+    if (!token) {
+      return res.json({
+        success: false,
+        message: "Not Authorized, Login Again",
+      });
     }
-}
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (decoded.email !== process.env.ADMIN_EMAIL) {
+      return res.json({
+        success: false,
+        message: "Not Authorized, Login Again",
+      });
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export default adminAuth;
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,18 +1,18 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 const authUser = async (req, res, next) => {
-    const token = req.headers.authorization?.split(" ")[1];
-    if (!token) {
-        return res.json({ success: false, message: "Not Authorized, Login Again" })
-    }
-    try {
-        const token_decode = jwt.verify(token, process.env.JWT_SECRET)
-        req.body.userId = token_decode.id
-        next()
-    } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: error.message })
-    }
-}
+  const token = req.headers.token || req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.json({ success: false, message: "Not Authorized, Login Again" });
+  }
+  try {
+    const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+    req.body.userId = token_decode.id;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export default authUser;
